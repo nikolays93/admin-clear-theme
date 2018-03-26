46acc7bed6e75dc167aedf127253408c9f29c7e6
Nickolays93 <nikolays93@ya.ru>
2018-03-26 10:50:03 +0400

    fix typos

---
+++ includes/clear-toolbar.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace CDevelopers\AdminClearTheme;
+
+if ( ! defined( 'ABSPATH' ) )
+    exit; // Exit if accessed directly
+
+function clear_toolbar( $wp_admin_bar ) {
+    $wp_admin_bar->remove_node( 'appearance' );
+    $wp_admin_bar->remove_node( 'comments' );
+    $wp_admin_bar->remove_node( 'updates' );
+    $wp_admin_bar->remove_node( 'wpseo-menu' ); // hide yost seo
+}
+
+function clear_yoast_from_toolbar() {
+    echo '<style>.yoast-seo-score.content-score,.yoast-seo-score.keyword-score,#wpseo-filter{display:none;}</style>';
+}
+
+if( ! Utils::get('clear_toolbar') ) {
+    add_action('admin_bar_menu', array(__NAMESPACE__ . '\clear_toolbar'), 666, 1);
+    add_action('admin_head', array(__NAMESPACE__ . '\clear_yoast_from_toolbar'));
+}
